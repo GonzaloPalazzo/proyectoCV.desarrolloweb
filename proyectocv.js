b27432d4a22e7c62bb2bc5e859559933a25edb9d
GonzaloPalazzo <126640309+GonzaloPalazzo@users.noreply.github.com>
2023-03-01 00:55:56 -0300

    Add files via upload

---
+++ proyectocv.js
@@ -0,0 +1,15 @@
+
+document.getElementById ('boton').onclick = function () {
+    console.log("mostramos los estudios");
+    document.getElementById("text1").innerHTML = "Colegio Nacional Buenos Aires 1999-2000. Abogado recibido en La Universidad Nacional de La Matanza 2006-2013"
+}
+
+document.getElementById ('boton2').onclick = function () {
+    console.log("mostramos Experiencias laborales");
+    document.getElementById("text2").innerHTML = "Procuracion en Estudio juridico Coudet 2013-2016. Secretario en la sindicatura general de la Nacion 2016-2023 "
+}
+
+document.getElementById ('boton3').onclick = function () {
+    console.log("mostramos conocimientos");
+    document.getElementById("text3").innerHTML = "Utilizacion de programa lexDoctor. Conocimientos en programas informaticos office. Manejo fluido de ingles oral y escrito avanzado"
+}
